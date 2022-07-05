c7313f42c653d9ee623227719b33c1ad058c41e4
Orlando Neris <orlandoneris24@gmail.com>
2022-07-05 19:45:34 -0300

    CAM-91

--- MICROSERVICIOS/MIC-CATALOGO/CAPA-DOMINIO/INTERFACE-QUERYS/REPOSITORIO-INTERFACE-QUERY.php
+++ MICROSERVICIOS/MIC-CATALOGO/CAPA-DOMINIO/INTERFACE-QUERYS/REPOSITORIO-INTERFACE-QUERY.php
@@ -2,4 +2,5 @@
 interface IRepositorioQuery{
     public function get_info_territorio($territorio_id);
     public function get_filtros($solapa,$lista_recursos_restringidos,$si_tengo_que_filtrar, $filtro_id);
+    public function get_filtros_activo();
 }
