8f10a826c59fd2fb5ca8aa959f7445b8bbf0cea3
Orlando Neris <orlandoneris24@gmail.com>
2022-07-07 12:58:52 -0300

    rft definiciones

--- MICROSERVICIOS/MIC-CATALOGO/CAPA-APLICACION/SERVICIOS/REPOSITORIO-SERVICIO.php
+++ MICROSERVICIOS/MIC-CATALOGO/CAPA-APLICACION/SERVICIOS/REPOSITORIO-SERVICIO.php
@@ -15,8 +15,8 @@ class RepositorioServicioCatalogo implements IRepositorioServicioCatalogo{
     }
 
 
-    public function get_filtros($solapa,$aux_cadena_filtros,$si_tengo_que_filtrar){
-        return $this->query->get_filtros($solapa,$aux_cadena_filtros,$si_tengo_que_filtrar);     //
+    public function get_filtros($solapa,$aux_cadena_filtros,$recursos_restringidos, $si_tengo_que_filtrar){
+        return $this->query->get_filtros($solapa,$aux_cadena_filtros,$recursos_restringidos, $si_tengo_que_filtrar);     //
     }
 
     public function get_filtros_activo()
--- MICROSERVICIOS/MIC-CATALOGO/CAPA-DOMINIO/INTERFACE-SERVICIO/REPOSITORIO-INTERFACE-SERVICIO.php
+++ MICROSERVICIOS/MIC-CATALOGO/CAPA-DOMINIO/INTERFACE-SERVICIO/REPOSITORIO-INTERFACE-SERVICIO.php
@@ -1,7 +1,7 @@
 <?php
  interface IRepositorioServicioCatalogo{
     public function get_info_territorio($territorio_id);
-    public function get_filtros($solapa,$aux_cadena_filtros,$lista_recursos_restringidos,$si_tengo_que_filtrar);
+    public function get_filtros($solapa,$aux_cadena_filtros,$recursos_restringidos, $si_tengo_que_filtrar);
     public function get_filtros_activo();
  }
  ?>
