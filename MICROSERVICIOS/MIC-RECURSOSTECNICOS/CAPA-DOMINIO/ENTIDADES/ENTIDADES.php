98482cda51eb9e6cdb2bc68fd0274b552ce763ee
Orlando Neris <orlandoneris24@gmail.com>
2022-07-11 19:09:58 -0300

    CAM-112

---
+++ MICROSERVICIOS/MIC-RECURSOSTECNICOS/CAPA-DOMINIO/ENTIDADES/ENTIDADES.php
@@ -0,0 +1,62 @@
+<?php
+require_once('C:/xampp/htdocs/atic/nuevo_repo/CambioArquitecturaEnarsa/MICROSERVICIOS/MIC-CATALOGO/CAPA-APLICACION/SERVICIOS/REPOSITORIO-SERVICIO.php');
+
+
+class Recurso{
+    public $solapa = 2;
+    public $origen_id = null;
+    public $id_recurso = null;
+    public $titulo = null;
+    public $descripcion = null;
+    public $link_imagen = null;
+    public $metatag = null;
+    public $autores = null;
+    public $estudios_id = null;
+    public $fecha = null;
+    public $tema = null;
+    public $ico = null;
+    public $estudios=array();
+    public $territorio_id = null;
+    public $territorio_desc = null;
+    public $territorio_fec_bbdd_date = null;
+    public $territorio_simpli = null;
+    public $territorio_fec_bbdd = null;
+
+    public function __construct($origen_id,$id_recurso,$titulo,$descripcion,$link_imagen,$metatag,$autores,$estudios_id,$fecha,$tema,$territorio_id,$ico){
+      
+        $this->origen_id = $origen_id;
+        $this->id_recurso = $id_recurso;
+        $this->titulo = $titulo;
+        $this->descripcion = $descripcion;
+        $this->link_imagen = $link_imagen;
+        $this->metatag = $metatag;
+        $this->autores = $autores;
+        $this->estudios_id = $estudios_id;
+        $this->fecha = $fecha;
+        $this->tema = $tema;
+        $this->ico = $ico;
+        $this->territorio_id = $territorio_id;
+        $this->territorio_desc;
+        $this->territorio_fec_bbdd_date;
+        $this->territorio_simpli;
+        $this->territorio_fec_bbdd;       
+
+        // si id_territorio es distinto de vacio, se llama al metodo para completar la informacion 
+        if($territorio_id != null){
+
+            $RepoCatalogo= new RepositorioServicioCatalogo();
+            $InfoTerritorio = $RepoCatalogo->get_info_territorio($this->territorio_id);
+            
+            $this->territorio_desc = $InfoTerritorio[0]['descripcion'];
+            $this->territorio_fec_bbdd_date = $InfoTerritorio[0]['fec_bbdd_date'];;
+            $this->territorio_simpli = $InfoTerritorio[0]['territorio_simpli'];
+            $this->territorio_fec_bbdd = $InfoTerritorio[0]['fec_bbdd'];
+
+        } 
+    }
+
+
+}
+
+?>
+
